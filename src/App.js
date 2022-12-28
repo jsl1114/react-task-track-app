52e784cb1b4325b52bc94c165c80529c613ef007
jsl1114 <jsl11141118@gmail.com>
2022-12-28 14:45:08 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const name = "Jason";
+  const cool = true;
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doc Appt',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false
+    },
+    {
+        id: 2,
+        text: 'grocery',
+        day: 'Feb 7th at 5:00pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Doc 2nd appt',
+        day: 'Feb 15th at 2:30pm',
+        reminder: false
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <h2>你好, {name}, 你是个{cool ? '酷人' : '傻逼'}</h2> */}
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = (props) => {
+    const onClick = () => {
+        console.log('Click');
+    }
+
+    return (
+        <header className='header'>
+            <h1>{props.title}</h1>
+            <Button color='green' text='Add' onClick ={onClick}  />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS in JS
+// const headingStyle = {
+//     color: 'red', backgroundColor:'cyan'
+// }
+
+export default Header
